0eed8537d3d43514a87108af4cefb4683ce62f1d
danielfromearth <daniel.kaufman@nasa.gov>
2023-08-08 12:06:45 -0400

    rename cli module to distinguish types

--- concatenator/concat_with_nco_cli.py
+++ concatenator/concat_with_nco_cli.py
@@ -1,5 +1,6 @@
 """A simple CLI wrapper around the main NCO concatenation function"""
 import logging
+import shutil
 import sys
 
 from concatenator.concat_with_nco import concat_netcdf_files
@@ -10,12 +11,15 @@ def run_nco_concat(args: list) -> None:
     """
     Parse arguments and run subsetter on the specified input file
     """
-    input_files, output_path = parse_args(args)
+    input_files, output_path, keep_tmp_files, temporary_dir_to_remove = parse_args(args)
     num_inputs = len(input_files)
 
     logging.info('Executing NCO concatenation on %d files...', num_inputs)
     concat_netcdf_files(input_files, output_path)
     logging.info('NCO concatenation complete. Result in %s', output_path)
+
+    if not keep_tmp_files and temporary_dir_to_remove:
+        shutil.rmtree(temporary_dir_to_remove)
 
 
 def main() -> None:
