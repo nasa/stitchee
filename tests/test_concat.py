dee76b820b772ddeb4231ea9697550f463e83b43
danielfromearth <daniel.kaufman@nasa.gov>
2023-08-08 11:10:34 -0400

    make record dimension a parameter for test functions, and expand to additional test data

--- tests/test_concat.py
+++ tests/test_concat.py
@@ -30,18 +30,20 @@ class TestConcat(TestCase):
         if not cls.KEEP_TMP:  # pylint: disable=no-member
             rmtree(cls.__output_path)
 
-    def run_verification_with_bumblebee(self, data_dir, output_name):
-        output_path = str(self.__output_path.joinpath(output_name))
-        data_path = self.__test_data_path.joinpath(data_dir)
+    def run_verification_with_bumblebee(self,
+                                        data_dir,
+                                        output_name,
+                                        record_dim_name: str = 'mirror_step'):
+        output_path = str(self.__output_path.joinpath(output_name))  # type: ignore
+        data_path = self.__test_data_path.joinpath(data_dir)  # type: ignore
 
         input_files = []
         for filepath in data_path.iterdir():
-            if Path(filepath).suffix == ".nc":
-                copied_input_new_path = self.__output_path / Path(filepath).name
+            if Path(filepath).suffix.lower() in (".nc", ".h5", ".hdf"):
+                copied_input_new_path = self.__output_path / Path(filepath).name  # type: ignore
                 shutil.copyfile(filepath, copied_input_new_path)
                 input_files.append(str(copied_input_new_path))
 
-        dim_for_record_dim = 'mirror_step'
         output_path = bumblebee(files_to_concat=input_files, output_file=output_path)
 
         merged_dataset = nc.Dataset(output_path)
@@ -50,10 +52,10 @@ class TestConcat(TestCase):
         #   the sum of the lengths across the input files
         length_sum = 0
         for file in input_files:
-            length_sum += len(nc.Dataset(file).variables[dim_for_record_dim])
-        assert length_sum == len(merged_dataset.variables[dim_for_record_dim])
+            length_sum += len(nc.Dataset(file).variables[record_dim_name])
+        assert length_sum == len(merged_dataset.variables[record_dim_name])
 
-    def run_verification_with_nco(self, data_dir, output_name):
+    def run_verification_with_nco(self, data_dir, output_name, record_dim_name='mirror_step'):
         output_path = str(self.__output_path.joinpath(output_name))
         data_path = self.__test_data_path.joinpath(data_dir)
 
@@ -64,9 +66,8 @@ class TestConcat(TestCase):
                 shutil.copyfile(filepath, copied_input_new_path)
                 input_files.append(str(copied_input_new_path))
 
-        dim_for_record_dim = 'mirror_step'
         concat_with_nco.concat_netcdf_files(input_files, output_path,
-                                            dim_for_record_dim=dim_for_record_dim,
+                                            dim_for_record_dim=record_dim_name,
                                             decompress_datasets=True)
 
         merged_dataset = nc.Dataset(output_path)
@@ -75,14 +76,34 @@ class TestConcat(TestCase):
         #   the sum of the lengths across the input files
         length_sum = 0
         for file in input_files:
-            length_sum += len(nc.Dataset(file).variables[dim_for_record_dim])
-        assert length_sum == len(merged_dataset.variables[dim_for_record_dim])
+            length_sum += len(nc.Dataset(file).variables[record_dim_name])
+        assert length_sum == len(merged_dataset.variables[record_dim_name])
 
     def test_tempo_no2_concat_with_bumblebee(self):
-        self.run_verification_with_bumblebee('tempo_no2', 'tempo_no2_concat_with_bumblebee.nc')
+        self.run_verification_with_bumblebee('tempo/tempo_no2',
+                                             'tempo_no2_concat_with_bumblebee.nc')
 
     def test_tempo_hcho_concat_with_bumblebee(self):
-        self.run_verification_with_bumblebee('tempo_hcho', 'tempo_hcho_concat_with_bumblebee.nc')
+        self.run_verification_with_bumblebee('tempo/tempo_hcho', 'tempo_hcho_concat_with_bumblebee.nc')
+
+    def test_tempo_cld04_concat_with_bumblebee(self):
+        self.run_verification_with_bumblebee('tempo/tempo_cld04', 'tempo_cld04_concat_with_bumblebee.nc')
+
+    def test_tempo_o3prof_concat_with_bumblebee(self):
+        self.run_verification_with_bumblebee('tempo/tempo_o3prof', 'tempo_o3prof_concat_with_bumblebee.nc')
+
+    def test_icesat_concat_with_bumblebee(self):
+        self.run_verification_with_bumblebee('icesat', 'icesat_concat_with_bumblebee.nc')
+
+    def test_ceres_concat_with_bumblebee(self):
+        self.run_verification_with_bumblebee('ceres-subsetter-output',
+                                             'ceres_concat_with_bumblebee.nc',
+                                             record_dim_name='time')
+
+    def test_ceres_flash_concat_with_bumblebee(self):
+        self.run_verification_with_bumblebee('ceres_flash-subsetter-output',
+                                             'ceres_flash_concat_with_bumblebee.nc',
+                                             record_dim_name='time')
 
     # def test_tempo_no2_concat_with_nco(self):
     #     self.run_verification_with_nco('tempo_no2', 'tempo_no2_concat_with_nco.nc')
