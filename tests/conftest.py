c80a4f33a92571651a65d0b48b1c7440adad24f4
danielfromearth <daniel.kaufman@nasa.gov>
2024-03-18 18:27:11 -0400

    rework temporary directories for tests and add toy data creation

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,123 @@
 """Initial configuration for tests."""
+
+import shutil
+import typing
+from collections.abc import Generator
+from pathlib import Path
+from tempfile import mkdtemp
+
+import netCDF4 as nc
 import pytest
+
+
+class DataDirs(typing.NamedTuple):
+    test_path: Path
+    test_data_path: Path
+
+
+class TempDirs(typing.NamedTuple):
+    output_path: Path
+    toy_data_path: Path
 
 
 def pytest_addoption(parser):
     """Sets up optional argument to keep temporary testing directory."""
     parser.addoption(
-        '--keep-tmp',
-        action='store_true',
-        help='Keep temporary directory after testing. Useful for debugging.')
+        "--keep-tmp",
+        action="store_true",
+        help="Keep temporary directory after testing. Useful for debugging.",
+    )
 
 
-@pytest.fixture(scope='class')
+@pytest.fixture(scope="class")
 def pass_options(request):
     """Adds optional argument to a test class."""
-    request.cls.KEEP_TMP = request.config.getoption('--keep-tmp')
+    request.cls.KEEP_TMP = request.config.getoption("--keep-tmp")
+
+
+@pytest.fixture(scope="session", autouse=True)
+def data_dirs() -> DataDirs:
+    __test_path = Path(__file__).parent.resolve()
+    __test_data_path = __test_path.joinpath("data")
+
+    return DataDirs(test_path=__test_path, test_data_path=__test_data_path)
+
+
+@pytest.fixture(scope="session", autouse=True)
+def temp_toy_data_dir(data_dirs) -> Generator[Path, None, None]:
+    # Setup
+    __toy_data_path = Path(mkdtemp(prefix="toy-", dir=data_dirs.test_data_path))
+    yield __toy_data_path
+
+    # Tear down
+    shutil.rmtree(__toy_data_path)
+
+
+@pytest.fixture(scope="function", autouse=True)
+def temp_output_dir(data_dirs) -> Generator[Path, None, None]:
+    # Setup
+    __output_path = Path(mkdtemp(prefix="tmp-", dir=data_dirs.test_data_path))
+    yield __output_path
+
+    # Tear down
+    shutil.rmtree(__output_path)
+
+
+def add_to_ds_3dims_3vars_4coords_1group_with_step_values(open_ds: nc.Dataset, step_values: list):
+    """Creates groups, dimensions, variables; and uses chosen step values in an open dataset"""
+    grp1 = open_ds.createGroup("Group1")
+
+    # Root-level Dimensions/Variables
+    open_ds.createDimension("step", 3)
+    open_ds.createDimension("track", 7)
+    open_ds.createVariable("step", "i2", ("step",), fill_value=False)
+    open_ds.createVariable("track", "i2", ("track",), fill_value=False)
+    open_ds.createVariable("var0", "f4", ("step", "track"))
+    #
+    open_ds["step"][:] = step_values
+    open_ds["track"][:] = [1, 2, 3, 4, 5, 6, 7]
+    open_ds["var0"][:] = [
+        [33, 78, 65, 12, 85, 35, 44],
+        [64, 24, 87, 12, 54, 82, 24],
+        [66, 18, 99, 52, 77, 88, 59],
+    ]
+
+    # Group 1 Dimensions/Variables
+    grp1.createDimension("level", 2)
+    grp1.createVariable("var1", "f8", ("step", "track"))
+    grp1.createVariable("var2", "f4", ("step", "track", "level"))
+    #
+    grp1["var1"][:] = [
+        [200, 300, 400, 500, 600, 700, 800],
+        [200, 300, 400, 500, 600, 700, 800],
+        [200, 300, 400, 500, 600, 700, 800],
+    ]
+    grp1["var2"][:] = [
+        [[200, 150], [300, 150], [400, 150], [500, 150], [600, 150], [700, 150], [800, 150]],
+        [[200, 150], [300, 150], [400, 150], [500, 150], [600, 150], [700, 150], [800, 150]],
+        [[200, 150], [300, 150], [400, 150], [500, 150], [600, 150], [700, 150], [800, 150]],
+    ]
+
+    return open_ds
+
+
+@pytest.fixture(scope="session")
+def ds_3dims_3vars_4coords_1group_part1(temp_toy_data_dir) -> Path:
+    filepath = temp_toy_data_dir / "test_3dims_3vars_4coords_1group_part1.nc"
+
+    f = nc.Dataset(filename=filepath, mode="w")
+    f = add_to_ds_3dims_3vars_4coords_1group_with_step_values(f, step_values=[9, 10, 11])
+    f.close()
+
+    return filepath
+
+
+@pytest.fixture(scope="session")
+def ds_3dims_3vars_4coords_1group_part2(temp_toy_data_dir):
+    filepath = temp_toy_data_dir / "test_3dims_3vars_4coords_1group_part2.nc"
+
+    f = nc.Dataset(filename=filepath, mode="w")
+    f = add_to_ds_3dims_3vars_4coords_1group_with_step_values(f, step_values=[12, 13, 14])
+    f.close()
+
+    return filepath
--- tests/test_concat.py
+++ tests/test_concat.py
@@ -4,9 +4,6 @@
 
 import shutil
 from pathlib import Path
-from shutil import rmtree
-from tempfile import mkdtemp
-from unittest import TestCase
 
 import netCDF4 as nc
 import pytest
@@ -15,35 +12,24 @@ from concatenator.stitchee import stitchee
 
 
 @pytest.mark.usefixtures("pass_options")
-class TestConcat(TestCase):
+class TestConcat:
     """Main concatenation testing class."""
-
-    @classmethod
-    def setUpClass(cls):
-        cls.__test_path = Path(__file__).parent.resolve()
-        cls.__test_data_path = cls.__test_path.joinpath("data")
-        cls.__output_path = Path(mkdtemp(prefix="tmp-", dir=cls.__test_data_path))
-
-    @classmethod
-    def tearDownClass(cls):
-        if not cls.KEEP_TMP:  # pylint: disable=no-member
-            rmtree(cls.__output_path)
 
     def run_verification_with_stitchee(
         self,
-        data_dir,
-        output_name,
+        input_dir: Path,
+        output_dir: Path,
+        output_name: str,
         concat_method: str = "xarray-concat",
         record_dim_name: str = "mirror_step",
         concat_kwargs: dict | None = None,
     ):
-        output_path = str(self.__output_path.joinpath(output_name))  # type: ignore
-        data_path = self.__test_data_path.joinpath(data_dir)  # type: ignore
+        output_path = str(output_dir.joinpath(output_name))  # type: ignore
 
         input_files = []
-        for filepath in data_path.iterdir():
+        for filepath in input_dir.iterdir():
             if Path(filepath).suffix.lower() in (".nc", ".h5", ".hdf"):
-                copied_input_new_path = self.__output_path / Path(filepath).name  # type: ignore
+                copied_input_new_path = output_dir / Path(filepath).name  # type: ignore
                 shutil.copyfile(filepath, copied_input_new_path)
                 input_files.append(str(copied_input_new_path))
 
@@ -69,19 +55,43 @@ class TestConcat(TestCase):
             length_sum += len(nc.Dataset(file).variables[record_dim_name])
         assert length_sum == len(merged_dataset.variables[record_dim_name])
 
-    def test_tempo_no2_concat_with_stitchee(self):
+    def test_simple_sample(
+        self,
+        temp_toy_data_dir,
+        temp_output_dir,
+        ds_3dims_3vars_4coords_1group_part1,
+        ds_3dims_3vars_4coords_1group_part2,
+    ):
         self.run_verification_with_stitchee(
-            "tempo/no2", "tempo_no2_bee_concatenated.nc", concat_method="xarray-concat"
+            input_dir=temp_toy_data_dir,
+            output_dir=temp_output_dir,
+            output_name="simple_sample_concatenated.nc",
+            record_dim_name="step",
+            concat_method="xarray-concat",
         )
 
-    def test_tempo_hcho_concat_with_stitchee(self):
+    def test_tempo_no2_concat_with_stitchee(self, data_dirs, temp_output_dir):
         self.run_verification_with_stitchee(
-            "tempo/hcho", "tempo_hcho_bee_concatenated.nc", concat_method="xarray-concat"
+            input_dir=data_dirs.test_data_path / "tempo/no2",
+            output_dir=temp_output_dir,
+            output_name="tempo_no2_stitcheed.nc",
+            concat_method="xarray-concat",
         )
 
-    def test_tempo_cld04_concat_with_stitchee(self):
+    def test_tempo_hcho_concat_with_stitchee(self, data_dirs, temp_output_dir):
         self.run_verification_with_stitchee(
-            "tempo/cld04", "tempo_cld04_bee_concatenated.nc", concat_method="xarray-concat"
+            input_dir=data_dirs.test_data_path / "tempo/hcho",
+            output_dir=temp_output_dir,
+            output_name="tempo_hcho_stitcheed.nc",
+            concat_method="xarray-concat",
+        )
+
+    def test_tempo_cld04_concat_with_stitchee(self, data_dirs, temp_output_dir):
+        self.run_verification_with_stitchee(
+            input_dir=data_dirs.test_data_path / "tempo/cld04",
+            output_dir=temp_output_dir,
+            output_name="tempo_cld04_stitcheed.nc",
+            concat_method="xarray-concat",
         )
 
     # def test_tempo_o3prof_concat_with_stitchee(self):
@@ -92,19 +102,21 @@ class TestConcat(TestCase):
     # def test_icesat_concat_with_stitchee(self):
     #     self.run_verification_with_stitchee('icesat', 'icesat_concat_with_stitchee.nc')
     #
-    def test_ceres_concat_with_stitchee(self):
+    def test_ceres_concat_with_stitchee(self, data_dirs, temp_output_dir):
         self.run_verification_with_stitchee(
-            "ceres-subsetter-output",
-            "ceres_bee_concatenated.nc",
+            input_dir=data_dirs.test_data_path / "ceres-subsetter-output",
+            output_dir=temp_output_dir,
+            output_name="ceres_bee_concatenated.nc",
             concat_method="xarray-combine",
             record_dim_name="time",
             concat_kwargs={"compat": "override", "combine_attrs": "override"},
         )
 
-    def test_ceres_flash_concat_with_stitchee(self):
+    def test_ceres_flash_concat_with_stitchee(self, data_dirs, temp_output_dir):
         self.run_verification_with_stitchee(
-            "ceres_flash-subsetter-output",
-            "ceres_flash_bee_concatenated.nc",
+            input_dir=data_dirs.test_data_path / "ceres_flash-subsetter-output",
+            output_dir=temp_output_dir,
+            output_name="ceres_flash_bee_concatenated.nc",
             concat_method="xarray-combine",
             record_dim_name="time",
             concat_kwargs={"compat": "override", "combine_attrs": "override"},
