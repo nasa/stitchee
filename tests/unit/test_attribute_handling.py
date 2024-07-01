23dc2fbfeb4aaf7471b0adbe392a6c87e73e3e2e
danielfromearth <daniel.kaufman@nasa.gov>
2024-07-01 16:37:26 -0400

    combine concatenator delim attribute unit tests, and reset values

--- tests/unit/test_attribute_handling.py
+++ tests/unit/test_attribute_handling.py
@@ -43,24 +43,28 @@ def test_coordinate_attribute_regrouping():
     )
 
 
-def test_concatenator_options_getting():
-    assert concatenator.group_delim == "__"
-    assert concatenator.coord_delim == "  "
-    assert concatenator.__options__.group_delim == "__"
-    with pytest.raises(AttributeError):
-        _ = concatenator.nonexistent_attribute
-
-
-def test_concatenator_options_setting():
+def test_concatenator_options_getting_and_setting():
     """Test setting attributes for the concatenator module.
 
     Note that currently, new attributes can be defined dynamically.
+    Note also, that attributes must be reset before further testing because they are global.
     """
+    default_group_delim = "__"
+    assert concatenator.group_delim == default_group_delim
     concatenator.group_delim = "%"
     assert concatenator.group_delim == "%"
+    concatenator.group_delim = default_group_delim
+    assert concatenator.group_delim == default_group_delim
 
+    default_coord_delim = "  "
+    assert concatenator.coord_delim == default_coord_delim
     concatenator.coord_delim = "---"
     assert concatenator.coord_delim == "---"
+    concatenator.group_delim = default_coord_delim
+    assert concatenator.group_delim == default_coord_delim
 
     concatenator.nonexistent_attribute = "---"
     assert concatenator.nonexistent_attribute == "---"
+    del concatenator.nonexistent_attribute
+    with pytest.raises(AttributeError):
+        _ = concatenator.nonexistent_attribute
